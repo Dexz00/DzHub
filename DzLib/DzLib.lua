247b7c5d9f0eaf775dae4fb57ee44180825052f3
Dexz <157737726+Dexz00@users.noreply.github.com>
2026-07-29 08:25:43 -0300

    Update DzLib.lua

--- DzLib/DzLib.lua
+++ DzLib/DzLib.lua
@@ -1,3 +1,7 @@
+if getgenv().Library then
+    getgenv().Library:Unload()
+end
+
 local Library do
     local Workspace = game:GetService("Workspace")
     local UserInputService = game:GetService("UserInputService")
@@ -7930,3 +7934,6 @@ function Library.Sections:AddLabel(TextOrData)
 
     return self:CreateLabel(TextOrData)
 end
+
+getgenv().Library = Library
+return Library
